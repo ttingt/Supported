afd841fd274e130bb162e725573926a6dc6f4309
Vincent Chan <vinnychan@users.noreply.github.com>
2015-06-27 15:57:09 -0700

    replicated watchface on main

--- src/app.js
+++ src/app.js
@@ -6,15 +6,54 @@
 
 var UI = require('ui');
 var Vector2 = require('vector2');
-
-var main = new UI.Card({
-  title: 'Pebble.js',
-  icon: 'images/menu_icon.png',
-  subtitle: 'Hello World!',
-  body: 'Press any button.'
+var Vibe = require('ui/vibe');
+var Check = new UI.Image({
+  position: new Vector2(10, 10),
+  size: new Vector2(10, 10),
+  image: 'checkmark10.png'
 });
 
+var timeText = new UI.TimeText({
+  position: new Vector2(0, 80),
+  size: new Vector2(144, 84),
+  text: "%H:%M",
+  font: 'bitham-42-bold',
+  color: 'white',
+  textAlign: 'center'
+});
+
+var rect = new UI.Rect({
+  position: new Vector2(0, 80),
+  size: new Vector2(144, 1),
+  backgroundColor: 'white'
+});
+
+var dateText = new UI.TimeText({
+  position: new Vector2(4, 50),
+  size: new Vector2(144, 30),
+  text: "%B %d",
+  font: 'gothic-24-bold',
+  color: 'white',
+  textAlign: 'left'
+});
+
+
+var main = new UI.Window({
+  
+//   title: timeText,
+//   icon: 'images/menu_icon.png',
+//   subtitle: 'Hello World!',
+//   body: 'Press any button.'
+   
+});
+
+main.add(timeText);
+main.add(dateText);
+main.add(rect);
+main.add(Check);
+
 main.show();
+Vibe.vibrate('long');
 
 main.on('click', 'up', function(e) {
   var menu = new UI.Menu({
@@ -53,8 +92,8 @@ main.on('click', 'select', function(e) {
 
 main.on('click', 'down', function(e) {
   var card = new UI.Card();
-  card.title('A Card');
-  card.subtitle('Is a Window');
-  card.body('The simplest window type in Pebble.js.');
+//   card.title('A Card');
+  card.subtitle('12:12');
+  card.body('June 27');
   card.show();
 });
